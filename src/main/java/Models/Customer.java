2ca804a36b470320570a06c598023fff6a79771c
Stefan Fankhauser <stefan.fankhauser@students.fhnw.ch>
2022-12-03 15:16:13 +0100

    Updated Customer classes

--- src/main/java/Models/Customer.java
+++ src/main/java/Models/Customer.java
@@ -1,4 +1,85 @@
 package Models;
 
 public class Customer {
+
+    public static int globalCount = 0;
+    private int id, plz;
+    private String firstName, lastName, street, location, email;
+    private boolean deleted;
+
+    // CONSTRUCTOR
+    public Customer(String firstName, String lastName, String street, int plz, String location, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.plz = plz;
+        this.location = location;
+        this.email = email;
+        this.id = globalCount++;
+    }
+
+    // GETTERS AND SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
--- src/main/java/Models/CustomerRepository.java
+++ src/main/java/Models/CustomerRepository.java
@@ -1,4 +1,44 @@
 package Models;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class CustomerRepository {
+
+    private List<Customer> customers = new ArrayList<>();
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public Customer getCustomerById(int id) {
+        for (Customer c : customers) {
+            if (c.getId() == id) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Saves customer to list
+     * If a customer with the same ID already exists, it will be replaced
+     */
+    public void saveCustomer(Customer c) {
+        Customer removeOld = null;
+        for (Customer cOld : customers) {
+            if (cOld.getId() == c.getId()) {
+                removeOld = cOld;
+                break;
+            }
+        }
+        if (removeOld != null)
+            customers.remove(removeOld);
+        customers.add(c);
+    }
+
+
 }
