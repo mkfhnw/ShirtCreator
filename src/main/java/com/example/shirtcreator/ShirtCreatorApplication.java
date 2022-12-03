7e6146a210a05afd26cfc96e508055f7562af482
mkfhnw <margareta.karaqi@students.fhnw.ch>
2022-12-03 14:10:02 +0100

    Created repository and added Readme

---
+++ src/main/java/com/example/shirtcreator/ShirtCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.shirtcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirtCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirtCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shirtcreator/ShirtCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shirtcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirtCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
