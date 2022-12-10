4665d7ce21a8e655b4c58490a7ce3cad84513d58
fabio <fabio.frey@students.fhnw.ch>
2022-12-10 12:33:24 +0100

    deleted TestData

--- src/main/java/com/example/shirtcreator/ShirtCreatorApplication.java
+++ src/main/java/com/example/shirtcreator/ShirtCreatorApplication.java
@@ -1,28 +1,17 @@
 package com.example.shirtcreator;
 
+import Persistence.Order;
+import Persistence.OrderRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShirtCreatorApplication {
 
-    /*@Autowired
-    TODO: Add TestData
-    private OrderRepository orderRepository;
-    */
-
     public static void main(String[] args) {
         SpringApplication.run(ShirtCreatorApplication.class, args);
     }
 
-    /* @PostConstruct
-    TODO: Add TestData
-    public void createTestData() {
-
-        orderRepository.addOrder(new Order(1, 1, 10));
-        orderRepository.addOrder(new Order(2, 2, 20));
-        orderRepository.addOrder(new Order(3, 3, 30));
-        orderRepository.addOrder(new Order(4, 4, 40));
-
-    }*/
 }
