51235e820d7548d079660e554971bd1bd1b47385
MadMowgli <joel.laeubin@juliusbaer.com>
2022-12-22 20:26:16 +0100

    Added panel controls in JS

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,34 @@
 // Globale Variablen für Attribute Configuration und Order
-var configCut = "";
-var configColor = "";
-var configSize = "";
-var configPattern = "";
-var configPrice = 0.0;
-var orderQuantity = 0;
-var orderShippingMethod = "";
-var orderPrice = 0.0;
+const configCut = "";
+const configColor = "";
+const configSize = "";
+const configPattern = "";
+const configPrice = 0.0;
+const orderQuantity = 0;
+const orderShippingMethod = "";
+const orderPrice = 0.0;
 
 $(document).ready(function () {
-    // initial die Panels orderDetailsDiv und afterSalesDiv ausblenden
-    $("#orderDetailsDiv").hide();
-    $("#afterSalesDiv").hide();
-});
 
-document.getElementById("btnOrder").addEventListener("click", (e) => {
-    $("#configurationDiv").hide();
-    $("#orderDetailsDiv").show();
+    // Event listeners
+    document.getElementById("btnOrder").addEventListener("click", (e) => {
+        document.getElementById("configuration-panel").classList.add("d-none");
+        document.getElementById("order-panel").classList.remove("d-none");
+    });
+
+    document.getElementById("btnBack").addEventListener("click", (e) => {
+        document.getElementById("order-panel").classList.add("d-none");
+        document.getElementById("configuration-panel").classList.remove("d-none");
+    });
+
+    document.getElementById("btnSubmit").addEventListener("click", (e) => {
+        document.getElementById("order-panel").classList.add("d-none");
+        document.getElementById("aftersales-panel").classList.remove("d-none");
+    });
+
+    document.getElementById("btnNewOrder").addEventListener("click", (e) => {
+        document.getElementById("aftersales-panel").classList.add("d-none");
+        document.getElementById("configuration-panel").classList.remove("d-none");
+    });
+
 });
